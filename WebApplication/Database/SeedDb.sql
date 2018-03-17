16b1c2de5a3d5cd07f122c871f4adbe9b6d82c76
Jacek Noga <jaceknoga11@gmail.com>
2018-03-17 13:34:33 +0100

    Added seed db script

---
+++ WebApplication/Database/SeedDb.sql
@@ -0,0 +1,8 @@
+﻿USE SupremeLiftDb
+
+/*
+INSERT INTO [Exercise] (Name, CaloriesBurned)
+	VALUES ('Back squat', 50), ('Deadlift', 45), ('Front squat', 45), ('Bench press', 20),
+	('Overhead press', 25), ('Barbell row', 30), ('Tricep dip', 20), ('Snatch', 40)
+*/
+
