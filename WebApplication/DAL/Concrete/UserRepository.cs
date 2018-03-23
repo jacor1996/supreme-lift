2038b85a13b77e58a8e8216f3275011b90ebfc8f
Jacek Noga <jaceknoga11@gmail.com>
2018-03-23 21:26:17 +0100

    Create data model from existing db

---
+++ WebApplication/DAL/Concrete/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Concrete
+{
+    class UserRepository : IUserRepository
+    {
+        public void Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
