1b3f9f4e6b011892954acdef9987b84f074c4cb2
Jacek Noga <jaceknoga11@gmail.com>
2018-05-18 20:59:03 +0200

    Added chart generator

--- WebApplication/WebApplication/Controllers/RecordController.cs
+++ WebApplication/WebApplication/Controllers/RecordController.cs
@@ -128,5 +128,10 @@ namespace WebApplication.Controllers
 
             return HttpNotFound("Something went wrong.");
         }
+
+        public ActionResult Chart()
+        {
+            return View();
+        }
     }
 }
