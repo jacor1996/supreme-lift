85ee6568e0b581caa6441241b80fbcc0ba4d2948
Jacek Noga <jaceknoga11@gmail.com>
2018-05-24 20:13:04 +0200

    Added strength controller

---
+++ WebApplication/WebApplication/Controllers/StrengthController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class StrengthController : Controller
+    {
+        // GET: Strength
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Squat()
+        {
+            return View();
+        }
+
+        public ActionResult Deadlift()
+        {
+            return View();
+        }
+
+        public ActionResult Bench()
+        {
+            return View();
+        }
+
+        public ActionResult Press()
+        {
+            return View();
+        }
+    }
+}
